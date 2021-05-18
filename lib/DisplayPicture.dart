2a5cbfc34dd1fd69fdec971dccaf6a203503bf6a
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-05-19 04:26:44 +0500

    📦 NEW : Display selected Image

--- lib/DisplayPicture.dart
+++ lib/DisplayPicture.dart
@@ -1,15 +1,36 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DisplayPicture extends StatefulWidget {
-  DisplayPicture({Key key}) : super(key: key);
-  @override
-  _DisplayPictureState createState() => _DisplayPictureState();
-}
+class DisplayPicture extends StatelessWidget {
+  DisplayPicture({Key key, this.image}) : super(key: key);
+  final File image;
 
-class _DisplayPictureState extends State<DisplayPicture> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Center());
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromRGBO(46, 25, 96, 1),
+              Color.fromRGBO(93, 16, 73, 1)
+            ])),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: MediaQuery.of(context).size.width / 3,
+                  backgroundImage: FileImage(image),
+                  backgroundColor: Colors.transparent,
+                )
+              ],
+            ))));
   }
 }
